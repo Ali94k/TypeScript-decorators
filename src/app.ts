78812d3a666d22c57c509d4475e107e4f7aacc27
Ali <alikerimli94@gmail.com>
2023-08-28 16:49:05 +0200

    Decorator for accessor and methods and parameters

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 function Logger(constructor: Function) {
-    console.log('Logging...')
-    console.log(constructor)
+  console.log('Logging...')
+  console.log(constructor)
 }
 
 // Decorator runs when class is defined, not when instantiated
 @Logger
 class Person {
-    name = 'Max'
+  name = 'Max'
 
-    constructor() {
-        console.log('Creating person object...')
-    }
+  constructor() {
+    console.log('Creating person object...')
+  }
 }
 
 const person = new Person()
@@ -19,35 +19,33 @@ console.log(person)
 // Decorator Factory
 
 function Logger2(logString: string) {
-    return function(constructor: Function) {
-        console.log(logString)
-        console.log(constructor)
-    }
+  return function (constructor: Function) {
+    console.log(logString)
+    console.log(constructor)
+  }
 }
 
 @Logger2('LOGGING - PERSON')
 class Person2 {
-    name = 'Max'
+  name = 'Max'
 
-    constructor() {
-        console.log('Creating person object...')
-    }
+  constructor() {
+    console.log('Creating person object...')
+  }
 }
-
 
 // Building more useful decorators
 
 function WithTemplate(template: string, hookId: string) {
-    return function(constructor: any) {
-        const hookEl = document.getElementById(hookId)
-        const p = new constructor() // new instance of the class !!! 
-        if (hookEl) {
-            hookEl.innerHTML = template
-            hookEl.querySelector('h1')!.textContent = p.name
-        }
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId)
+    const p = new constructor() // new instance of the class !!!
+    if (hookEl) {
+      hookEl.innerHTML = template
+      hookEl.querySelector('h1')!.textContent = p.name
     }
+  }
 }
-
 
 // Multiple Decorators
 // Order of creation of decorators is top down
@@ -55,44 +53,95 @@ function WithTemplate(template: string, hookId: string) {
 @Logger2('LOGGING - PERSON')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person3 {
-    name = 'Max'
+  name = 'Max'
 
-    constructor() {
-        console.log('Creating person object...')
-    }
+  constructor() {
+    console.log('Creating person object...')
+  }
 }
 
 // Decorator for property
 
 function Log(target: any, propertyName: string | Symbol) {
-    console.log('Property decorator!')
-    console.log(target, propertyName)
+  console.log('Property decorator!')
+  console.log(target, propertyName)
 }
 
 class Product {
-    @Log
-    title: string
-    private _price: number
+  @Log
+  title: string
+  private _price: number
 
-    set price(val: number) {
-        if (val > 0) {
-            this._price = val
-        }
-        else {
-            throw new Error('Invalid price - should be positive!')
-        }
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val
+    } else {
+      throw new Error('Invalid price - should be positive!')
     }
+  }
 
-    constructor(t: string, p: number) {
-        this.title = t
-        this._price = p
-    }
+  constructor(t: string, p: number) {
+    this.title = t
+    this._price = p
+  }
 
-    getInformation() {
-        return [this.title, `$${this.price}`]
-    }
+  getInformation() {
+    return [this.title, `$${this.price}`]
+  }
 
-    priceWithTax(tax: number) {
-        return this._price * (1 + tax)
+  priceWithTax(tax: number) {
+    return this._price * (1 + tax)
+  }
+}
+
+// Decorator for accessor and methods and parameters
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Method decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
+class Product2 {
+  @Log
+  title: string
+  private _price: number
+
+  constructor(t: string, p: number) {
+    this.title = t
+    this._price = p
+  }
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val
+    } else {
+      throw new Error('Invalid price - should be positive!')
     }
+  }
+
+  @Log3
+  priceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax)
+  }
 }
